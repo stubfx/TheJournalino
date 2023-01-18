1581b5096057a6956cc99386ec9b759877716941
stubfx <14958556+stubfx@users.noreply.github.com>
2023-01-18 20:07:49 +0100

    enable debug

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const fastify = fastify0({
     // Set this to true for detailed logging:
-    logger: false,
+    logger: true,
 });
 
 // Setup our static files
