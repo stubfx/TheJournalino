bc93263fb0b77a855b4866d982b3000b4e16a160
stubfx <14958556+stubfx@users.noreply.github.com>
2023-01-24 12:33:15 +0100

    Remove "+" symbol in topics

--- datamodels/topicsData.js
+++ datamodels/topicsData.js
@@ -1,132 +1,143 @@
 export default {
     "top": {
         name: 'Top News',
-        queries: ["top+news"]
+        queries: ["top news"]
     },
     "gaming": {
         name: 'Gaming',
         queries: [
             "SeaOfThieves",
-            "Overwatch+2",
-            "Rainbow+six+siege+game",
-            "Valve+Steam+games",
-            "Warzone+2",
+            "Overwatch 2",
+            "Rainbow six siege game",
+            "Valve Steam games",
+            "Warzone 2",
             "Videogames",
-            "Counter+strike",
+            "Counter strike",
             "Valorant",
-            "League+of+Legends",
+            "League of Legends",
         ]
     },
     "movies&series": {
         name: 'Movies and Series',
         queries: [
-            "Netflix+upcoming",
-            "Christopher+Nolan+new+movie",
-            "War+movie",
-            "Marvel+cinematic+universe",
-            "DC+cinematic+universe",
-            "HBO+new+serie",
-            "Pixar+movie",
-            "Disney+plus+upcoming",
-            "Quentin+Tarantino",
-            "Upcoming+movie+leak",
-            "Tom+Cruise"
+            "Netflix upcoming",
+            "Christopher Nolan new movie",
+            "War movie",
+            "Marvel cinematic universe",
+            "DC cinematic universe",
+            "HBO new serie",
+            "Pixar movie",
+            "Disney plus upcoming",
+            "Quentin Tarantino",
+            "Upcoming movie leak",
+            "Tom Cruise"
         ]
     },
     "tech": {
         name: 'Tech',
         queries: [
-            "tech+news",
-            "Intel+chip",
-            "AMD+chip",
-            "Nvidia+GPU",
-            "Nvidia+AI"
+            "tech news",
+            "Intel chip",
+            "AMD chip",
+            "Nvidia GPU",
+            "Nvidia AI"
         ]
     },
     "stocks": {
         name: 'Stocks',
         queries: [
-            "Google+Market+Stocks",
-            "Intel+Market+Stocks",
-            "AMD+chip+Market+Stocks",
-            "Nvidia+Market+Stocks",
-            "Nvidia+Market+Stocks"
+            "Google Market Stocks",
+            "Intel Market Stocks",
+            "AMD chip Market Stocks",
+            "Nvidia Market Stocks",
+            "Nvidia Market Stocks"
         ]
     },
     "crypto": {
         name: 'Crypto',
         queries: [
             "cryptocurrencies",
-            "crypto+currencies",
+            "crypto currencies",
             "Bitcoin",
-            "new+crypto+asset"
+            "new crypto asset"
         ]
     },
     "anime&manga": {
         name: 'anime&manga',
         queries: [
-            "anime+magazine",
-            "new+anime",
-            "popular+anime",
-            "manga+magazine",
-            "new+manga",
-            "popular+manga",
+            "anime magazine",
+            "new anime",
+            "popular anime",
+            "manga magazine",
+            "new manga",
+            "popular manga",
+        ]
+    },
+    "minecraft": {
+        name: 'Minecraft',
+        queries: [
+            "Minecraft Java leaks",
+            "Minecraft Java new update",
+            "Minecraft Java new exploit",
+            "Minecraft BedRock leaks",
+            "Minecraft BedRock new update",
+            "Minecraft BedRock new exploit",
         ]
     },
     "r6s": {
         name: 'Rainbow six Siege',
         queries: [
-            "Rainbow+six+siege+operator",
-            "Rainbow+six+siege+map",
-            "Rainbow+six+siege+tournament",
-            "Rainbow+six+siege+official",
-            "Rainbow+six+siege+multiplayer"
+            "Rainbow six siege operator",
+            "Rainbow six siege map",
+            "Rainbow six siege tournament",
+            "Rainbow six siege official",
+            "Rainbow six siege multiplayer"
         ]
     },
     "lol": {
         name: 'League Of Legends',
         queries: [
             "Leagueoflegends",
-            "Leagueoflegends+new",
-            "Leagueoflegends+leaks",
-            "Leagueoflegends+champion"
+            "Leagueoflegends new",
+            "Leagueoflegends leaks",
+            "Leagueoflegends champion"
         ]
     },
     "war": {
         name: 'War',
         queries: [
-            "Ukraine+War",
-            "War+Crime",
-            "America+War",
-            "Military+mission",
-            "Strategic+Military+war"
+            "Ukraine War",
+            "War Crime",
+            "America War",
+            "Military mission",
+            "Strategic Military war"
         ]
     },
     "crimes": {
         name: 'Crimes',
         queries: [
-            "Drunk+driver+crime",
-            "Pursuit+crime",
-            "Escaped+from+prison+fugitive",
-            "Stolen+car+crime"
+            "Drunk driver crime",
+            "Pursuit crime",
+            "Escaped from prison fugitive",
+            "Stolen car crime"
         ]
     },
     "lgbt": {
         name: 'LGBT',
         queries: [
             "lgbt",
-            "lgbt+protest",
-            "lgbt+improvements",
-            "lgbt+rights"
+            "lgbt protest",
+            "lgbt improvements",
+            "lgbt rights"
         ]
     },
     "potato": {
         name: 'Potato',
         queries: [
-            "harvesting+potatoes",
-            "cooking+with+potatoes",
-            "potato+dish",
-            "potato+battery"
+            "harvesting potatoes",
+            "cooking with potatoes",
+            "potato dish",
+            "potato battery"
         ]
     }
 }
