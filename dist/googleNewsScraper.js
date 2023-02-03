fd97107105e48263f84c783f42658b6298614a03
stubfx <14958556+stubfx@users.noreply.github.com>
2023-02-03 10:04:38 +0100

    test puppeteer

--- dist/googleNewsScraper.js
+++ dist/googleNewsScraper.js
@@ -6,15 +6,15 @@ const puppeteerConfig = {
     args: puppeteer.defaultArgs().concat(['--no-sandbox', '--disable-setuid-sandbox'])
 };
 const browser = await puppeteer.launch(puppeteerConfig);
+const page = await browser.newPage();
+await page.setViewport({ width: 1366, height: 768 });
+await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36');
+await page.setCookie({
+    name: "CONSENT",
+    value: `YES+cb.${new Date().toISOString().split('T')[0].replace(/-/g, '')}-04-p0.en-GB+FX+667`,
+    domain: ".google.com"
+});
 export async function scrapeThis(url) {
-    const page = await browser.newPage();
-    await page.setViewport({ width: 1366, height: 768 });
-    await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36');
-    await page.setCookie({
-        name: "CONSENT",
-        value: `YES+cb.${new Date().toISOString().split('T')[0].replace(/-/g, '')}-04-p0.en-GB+FX+667`,
-        domain: ".google.com"
-    });
     await page.goto(url, { waitUntil: 'networkidle2' });
     let newUrl = page.url();
     let pageHtml = await page.content();
